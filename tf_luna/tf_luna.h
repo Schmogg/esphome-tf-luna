b75fd8eb3c629864a2418145dce2d28a380406f4
Schmogg <33796824+Schmogg@users.noreply.github.com>
2025-06-12 00:35:36 +0200

    Update tf_luna.h

--- tf_luna/tf_luna.h
+++ tf_luna/tf_luna.h
@@ -1,1 +1,23 @@
+#pragma once
+#include "esphome.h"
 
+class TFLunaSensor : public PollingComponent, public UARTDevice, public Sensor {
+ public:
+  TFLunaSensor(UARTComponent *parent) : UARTDevice(parent), PollingComponent(1000) {}
+
+  void update() override {
+    const int frame_size = 9;
+    uint8_t buffer[frame_size];
+
+    if (available() >= frame_size) {
+      while (available() >= frame_size) {
+        read_array(buffer, frame_size);
+
+        if (buffer[0] == 0x59 && buffer[1] == 0x59) {
+          uint16_t distance = buffer[2] + (buffer[3] << 8);
+          publish_state(distance);
+        }
+      }
+    }
+  }
+};
